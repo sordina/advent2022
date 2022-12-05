e9fa07c6f66d42b355b65f81942d9afd3a7bca15
Lyndon Maydwell <lyndon@sordina.net>
2022-12-05 13:07:13 +1000

    1b

--- src/Advent01.hs
+++ src/Advent01.hs
@@ -2,10 +2,14 @@
 
 module Advent01 where
 
+import Data.List
 import Data.List.Split
 
 day1 :: String -> Integer
 day1 = maximum . map (sum . map (read @Integer)) . splitOn [""] . lines
+
+day1b :: String -> Integer
+day1b = sum . take 3 . reverse . sort . map (sum . map (read @Integer)) . splitOn [""] . lines
 
 -- | Testing against test_input
 --
--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,7 @@ interactShow f = interact ((++ "\n") . show . f)
 days :: [(String, IO ())]
 days =
     [ ("01",  interactShow Advent01.day1)
+    , ("01b", interactShow Advent01.day1b)
     ]
 
 help :: a
